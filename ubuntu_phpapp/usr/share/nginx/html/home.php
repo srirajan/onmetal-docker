b8c6cb1951e828cd78f1dd86564bba69c9f1fdae
srirajan <sriram.rajan@gmail.com>
2014-10-26 16:41:59 +0000

    some tweaks to the output

--- ubuntu_phpapp/usr/share/nginx/html/home.php
+++ ubuntu_phpapp/usr/share/nginx/html/home.php
@@ -5,7 +5,7 @@
 <?php
 echo "<strong>There is no place like 127.0.0.1</strong>";
 echo "<br/>";
-echo "Date & Time:" . date('Y-m-d H:i:s');
+echo "Date & Time: " . date('Y-m-d H:i:s');
 echo "<br/>";
 echo "Container name: " . gethostname();
 echo "<hr/>"
