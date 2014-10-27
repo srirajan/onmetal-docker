f432d4dfe6788bc70317e148d9d1158df7450146
root <sriram.rajan@gmail.com>
2014-10-27 18:27:13 +0000

    deleting unwanted stuff

--- dbhelper/usr/local/bin/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mysql -e "create database if not exists ${DB_NAME}"
-curl -o /tmp/world_innodb.sql https://raw.githubusercontent.com/RackspaceDevOps/demos/master/mysql_db/world_innodb.sql
-mysql -h 127.0.0.1 -P  3306 ${DB_NAME} < /tmp/world_innodb.sql
-mysql -h 127.0.0.1 -P  3306 -e "GRANT ALL on ${DB_NAME}.* to '${DB_USER}'@'%'"
-mysql -h 127.0.0.1 -P  3306 -e "SET PASSWORD for '${DB_USER}@'%' = PASSWORD('${DB_PW}')"
