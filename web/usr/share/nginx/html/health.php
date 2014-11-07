c36649f5ac39baf0b0d139e1723601a285283303
srirajan <sriram.rajan@gmail.com>
2014-11-07 14:09:10 +0000

    fixing php errors

--- web/usr/share/nginx/html/health.php
+++ web/usr/share/nginx/html/health.php
@@ -3,14 +3,14 @@ include_once "db.php";
 
 $link = mysql_connect( $db_host, $db_user, $db_password)
 if (!$link) {
-	echo "FAIL"
+	echo "FAIL";
 }
 else {
  if (! mysql_select_db($db_name)) {
- 	echo "FAIL"
+ 	echo "FAIL";
  }
  else {
- 	echo "PASS"
+ 	echo "PASS";
  } 
 }
 ?>
