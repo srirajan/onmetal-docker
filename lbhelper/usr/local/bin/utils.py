fb2d9bcc0d00b59e9fcc800092711c616dec5782
srirajan <sriram.rajan@gmail.com>
2014-11-09 18:42:02 +0000

    removed colour printing

--- lbhelper/usr/local/bin/utils.py
+++ lbhelper/usr/local/bin/utils.py
@@ -21,23 +21,8 @@ import os
 import config
 import string
     
-color_debug = "yellow"
-color_error = "red"
-color_pass = "green"
-
-
 def log_msg(msg, type, fileobj):
-    if config.print_color:
-        if type == "ERROR" or type == "FAIL":
-            print_color = "red"
-        if type == "PASS" or type == "NORMAL" or type == "INFO":
-            print_color = "green"
-        if type == "DEBUG":
-            print_color = "yellow"
-        print(colored("".join(["[", datetime.datetime.now().strftime('%x %X'), "]:", msg]),
-                      print_color), file=fileobj)
-    else:
-        print("".join(["[", datetime.datetime.now().strftime('%x %X'), "]",
-                              "[", type, "]:", msg])
-                      , file=fileobj)
+      print("".join(["[", datetime.datetime.now().strftime('%x %X'), "]",
+                            "[", type, "]:", msg])
+                    , file=fileobj)
 
