5c066788c790af27474fdcf60284249bb5c3b09a
srirajan <sriram.rajan@gmail.com>
2014-11-09 18:21:57 +0000

    fixing missing semicolon

--- web/usr/share/nginx/html/health.php
+++ web/usr/share/nginx/html/health.php
@@ -1,7 +1,8 @@
 <?php
 include_once "db.php";
 
-$link = mysql_connect( $db_host, $db_user, $db_password)
+$link = mysql_connect( $db_host, $db_user, $db_password);
+
 if (!$link) {
 	echo "FAIL";
 }
