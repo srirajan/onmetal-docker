030aaac6afa07ec66bad07dbd6d781efbda22cb0
srirajan <sriram.rajan@gmail.com>
2014-11-12 17:01:58 +0000

    added comments

--- web/usr/local/bin/start.sh
+++ web/usr/local/bin/start.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+# THis is needed to query etcd
 ROUTER_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-
 echo $ROUTER_IP etcd_host >> /etc/hosts
 
 /usr/local/bin/configuredb.sh
 
-# start all the services
 /usr/local/bin/supervisord -n
