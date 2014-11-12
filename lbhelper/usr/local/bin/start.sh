06c0bac7e2c9f94560a898cfe35c6a40f5774cda
srirajan <sriram.rajan@gmail.com>
2014-11-12 13:13:50 +0000

    added comments

--- lbhelper/usr/local/bin/start.sh
+++ lbhelper/usr/local/bin/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+# this is needed for the script to communicate with etcd
 ROUTER_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-
 echo $ROUTER_IP etcd_host >> /etc/hosts
 
 /usr/bin/python /usr/local/bin/lbmanage.py
